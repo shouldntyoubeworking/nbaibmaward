b5f3bee082800fbda05417c2b555fb1042f24248
WatchOutIGotYou <185667804+shouldntyoubeworking@users.noreply.github.com>
2024-10-22 11:52:17 -0700

    Formula Fix

--- src/Formula.tsx
+++ src/Formula.tsx
@@ -5,6 +5,7 @@ export default function Formula(props: any) {
                 player_counting = (player_pts - player_fga + player_reb + player_ast + player_stl + player_blk - player_pf - player_to) <br />
                 player_score = (player_counting + (team_wins * 10)) * 250<br />
                 team_score = team_pts - team_fga + team_reb + team_ast + team_stl + team_blk - team_pf - team_to<br />
+                ibm_score = player_score / team_score
         </div>
 
     </div>
